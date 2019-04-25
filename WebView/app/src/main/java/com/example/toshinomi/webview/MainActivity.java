af9d7893443d2b57ad9a9f9fdd26b77dd4e2774c
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-04-25 22:34:44 +0900

    新規作成

--- WebView/app/src/main/java/com/example/toshinomi/webview/MainActivity.java
+++ WebView/app/src/main/java/com/example/toshinomi/webview/MainActivity.java
@@ -2,12 +2,139 @@ package com.example.toshinomi.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.webkit.WebView;
+import android.widget.EditText;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    final private String m_strUri = "https://www.google.com/";
+    //final private String m_strUri = "";
+    private Button m_btnBack;
+    private Button m_btnForward;
+    private Button m_btnReload;
+    private EditText m_textUri;
+    private WebView m_webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitLayout();
+
+        HomeWebView();
+    }
+
+    public void InitLayout() {
+        m_btnBack = findViewById(R.id.buttonBack);
+        m_btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BackWebView();
+            }
+        });
+
+        m_btnForward = findViewById(R.id.buttonForward);
+        m_btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FowardWebView();
+            }
+        });
+
+        m_btnReload = findViewById(R.id.buttonReload);
+        m_btnReload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReloadWebView();
+            }
+        });
+
+        m_textUri = findViewById(R.id.edittextUri);
+        m_textUri.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent e) {
+                if((e.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    m_webView.loadUrl(m_textUri.getText().toString());
+                }
+
+                return true;
+            }
+        });
+
+        m_webView = (WebView)findViewById(R.id.webView1);
+    }
+
+    public Boolean BackWebView()
+    {
+        Boolean bRst = true;
+        try
+        {
+            m_webView.goBack();
+        }
+        catch (Exception e)
+        {
+            bRst = false;
+        }
+
+        return bRst;
+    }
+
+    public Boolean FowardWebView()
+    {
+        Boolean bRst = true;
+        try
+        {
+            m_webView.goForward();
+        }
+        catch (Exception e)
+        {
+            bRst = false;
+        }
+
+        return bRst;
+    }
+
+    public Boolean ReloadWebView()
+    {
+        Boolean bRst = true;
+        try
+        {
+            m_webView.reload();
+        }
+        catch (Exception e)
+        {
+            bRst = false;
+        }
+
+        return bRst;
+    }
+
+    public Boolean HomeWebView()
+    {
+        m_textUri.setText(m_strUri);
+        String strUri = m_strUri;
+        return ShowWebView(strUri);
+    }
+
+    public Boolean ShowWebView(String _strUri)
+    {
+        Boolean bRst = true;
+        try
+        {
+            m_webView.loadUrl(_strUri.toString());
+            m_textUri.setText(_strUri.toString());
+        }
+        catch (Exception e)
+        {
+            bRst = false;
+        }
+
+        return bRst;
     }
 }
